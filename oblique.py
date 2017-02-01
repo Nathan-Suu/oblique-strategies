23e34a535ddd9d6a37caa158732214e2f7fae653
Chris McDowall <chris.mcdowall@gmail.com>
2017-02-01 16:56:08 +1300

    Added multi-line

--- oblique.py
+++ oblique.py
@@ -3,33 +3,50 @@ import random
 # TODO - handle/wrap cards longer than the card width
 # TODO - specify card width from console
 
-def display(card, card_width=60):
-    print('\n+{}+'.format('-' * card_width))
-    print('|{}|'.format(' ' * card_width))
-    print('|{}|'.format(' ' * card_width))
-    print('|{}|'.format(' ' * card_width))
-    print('|{}|'.format(str.center(card, card_width)))
-    print('|{}|'.format(' ' * card_width))
-    print('|{}|'.format(' ' * card_width))
-    print('|{}|'.format(' ' * card_width))
-    print('+{}+\n'.format('-' * card_width))
+def blank_line(card_width, repeats=1):
+    for repeat in range(repeats):
+        print('|{}|'.format(' ' * card_width))
 
-def draw(card_width=60):
+def card_edge(card_width):
+    print('\n+{}+'.format('-' * card_width))
+
+def display(card, card_width=40):
+    card_edge(card_width)
+    blank_line(card_width, repeats=3)
+
+    # print the message
+    for line in card:
+        print('|{}|'.format(str.center(line, card_width)))
+    if len(card) == 1:
+        blank_line(card_width)
+
+    blank_line(card_width, repeats=3)
+    card_edge(card_width)
+
+def draw_card(text_width=30):
     with open('strategies.txt', 'r') as f:
         rows = f.readlines()
-
     cards = [row.strip() for row in rows]
-    card = None
 
-    while not card:
-        card = random.choice(cards) 
-        if len(card) >= card_width:
-            card = None
+    card = []
+    line = ''
+
+    words = random.choice(cards).split(' ')
+    while words:
+        word = words.pop(0)
+        newline = line + ' ' + word
+        if len(newline) > text_width:
+            card.append(line.strip())
+            line = word
+        else:
+            line = newline.strip()
+    card.append(line)
+
     return card
 
 
 def main():
-    card = draw()
+    card = draw_card()
     display(card)
 
 if __name__ == '__main__':
