46c429112fb14ad30beb20c7113f93a4b95eb5b6
Chris McDowall <chris.mcdowall@gmail.com>
2017-02-01 22:05:48 +1300

    Punctuation breaks lines

--- oblique.py
+++ oblique.py
@@ -5,6 +5,10 @@ import random
 
 def blank_line(card_width, repeats=1):
     for repeat in range(repeats):
+        print('')
+
+def card_line(card_width, repeats=1):
+    for repeat in range(repeats):
         print('|{}|'.format(' ' * card_width))
 
 def card_edge(card_width, pad_start=False, pad_end=False):
@@ -15,38 +19,62 @@ def card_edge(card_width, pad_start=False, pad_end=False):
         print('')
 
 def display(card, card_width=40):
-    card_edge(card_width, pad_start=True)
+    '''Display a card on the console'''
     blank_line(card_width, repeats=3)
+    card_edge(card_width, pad_start=True)
+    card_line(card_width, repeats=3)
 
     # print the message
     for line in card:
         print('|{}|'.format(str.center(line, card_width)))
     if len(card) == 1:
-        blank_line(card_width)
+        card_line(card_width)
 
-    blank_line(card_width, repeats=3)
+    card_line(card_width, repeats=3)
     card_edge(card_width, pad_end=True)
+    blank_line(card_width, repeats=2)
 
 def draw_card(text_width=30):
+    '''Draw a card from the deck.'''
     with open('strategies.txt', 'r') as f:
         rows = f.readlines()
     cards = [row.strip() for row in rows]
 
+    # Set up empty card.
+    # It will become an array of strings to print on the card.
+    # e.g. ['This is first line,', 'and this is second']
     card = []
     line = ''
 
+    # get a random line of words
     words = random.choice(cards).split(' ')
+    # turn the raw words into rows
     while words:
         word = words.pop(0)
+        # check that it's a word
+        if not word.strip():
+            continue
         newline = line + ' ' + word
-        if len(newline) > text_width:
+        # break line if too long or starts with certain character
+        if len(newline) > text_width or word[0] in ['(', '-']:
             card.append(line.strip())
             line = word
         else:
             line = newline.strip()
+        # break line if ends with certain character
+        if word[-1] in ['?', ';', ',', '.', '(']:
+            card.append(line.strip())
+            line = ''
+
     card.append(line)
 
     return card
+
+def draw_many(draws=3):
+    '''Draw multiple cards'''
+    for counter in range(draws):
+        print('{} of {}'.format(counter + 1, draws))
+        main()
 
 
 def main():
@@ -55,3 +83,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    #draw_many(draws=3)
